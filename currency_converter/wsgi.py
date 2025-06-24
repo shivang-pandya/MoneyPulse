a740e88239d44d771c8dc6f4bbbfffb930f6fa5b
shivang-pandya <155375779+shivang-pandya@users.noreply.github.com>
2025-06-24 22:20:53 +0530

    Add files via upload

--- currency_converter/wsgi.py
+++ currency_converter/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currency_converter.settings')
 
 application = get_wsgi_application()
+
+app = application
