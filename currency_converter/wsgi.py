4420dd0e1680fa2e0b309d8626a2e4fd5b9d9f7d
shivang-pandya <shivangpandya6212@gmail.com>
2025-06-25 14:06:39 +0530

    vercel final

--- currency_converter/wsgi.py
+++ currency_converter/wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 WSGI config for currency_converter project.
 
@@ -16,23 +16,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currency_converter.settings')
 
 application = get_wsgi_application()
 
-=======
-"""
-WSGI config for currency_converter project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currency_converter.settings')
-
-application = get_wsgi_application()
-
->>>>>>> 7b25173f83cc42e71a57b3263d2066a519c71c28
 app = application
